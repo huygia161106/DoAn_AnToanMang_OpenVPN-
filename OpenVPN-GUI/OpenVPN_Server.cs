7aa59bb42cd27f29764956d588d9a6f16d540aa4
HoHuy1503 <htbaohuy2006@gmail.com>
2026-04-14 12:02:20 +0700

    Add files via upload

---
+++ OpenVPN-GUI/OpenVPN_Server.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenVPN_GUI
+{
+    public partial class OpenVPN_Server : Form
+    {
+        public OpenVPN_Server()
+        {
+            InitializeComponent();
+        }
+    }
+}
